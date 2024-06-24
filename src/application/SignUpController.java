1d02762b96eaec90820a296fa227ca9f708bc8c3
Taanishq <taanishqsethi28@gmail.com>
2024-06-24 13:03:41 -0700

    Updated the SignUpController

--- src/application/SignUpController.java
+++ src/application/SignUpController.java
@@ -1,5 +1,37 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 public class SignUpController {
+
+    @FXML
+    private TextField userNameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private PasswordField confirmPasswordField;
+
+    @FXML
+    private Button signUpButton;
+
+    @FXML
+    private void handleSingUp(){
+        String username = userNameField.getText();
+        String password = passwordField.getText();
+        String confirmPassword = confirmPasswordField.getText();
+
+        //Checks is password and confirm password are equal
+        if(password.equals(confirmPassword)){
+            //Registration Logic here
+            System.out.println("Password Matched");
+        }else{
+            //Show Error Message
+            System.out.println("Passwords do not match");
+        }
+    }
 
 }
