de44dd476d94480456cbf9a04dd892cb037e8c70
Taanishq <taanishqsethi28@gmail.com>
2024-07-08 17:49:34 -0700

    small edits

--- src/application/controller/WelcomePageController.java
+++ src/application/controller/WelcomePageController.java
@@ -20,6 +20,12 @@ public class WelcomePageController {
     private PasswordField passwordField;
 
     @FXML
+    private Button signInButton;
+
+    @FXML
+    private Button signUpButton;
+
+    @FXML
     private void initialize() {
         // Initialization code here
 
